1cfe1da0c5c829fc4bdfe51724d193cbf2341599
Gesiel Lucas <gesiel.lucas@hotmail.com>
2023-01-24 10:34:21 -0300

    Getting initial setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <form @submit.prevent="addTodo">
+      <input v-model="newTodo" placeholder="Add a todo"/>
+      <button>Add</button>
+    </form>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.text }}
+        <button @click="removeTodo(todo.id)">X</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodo: '',
+      todos: [
+        { id: 1, text: 'Learn Vue.js' },
+        { id: 2, text: 'Build a todo app' },
+      ]
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length + 1,
+        text: this.newTodo
+      });
+      this.newTodo = '';
+    },
+    removeTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
+  }
+}
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+      <form @submit.prevent="login">
+        <label for="username">Username</label>
+        <input v-model="username" id="username" type="text"/>
+        <label for="password">Password</label>
+        <input v-model="password" id="password" type="password"/>
+        <button>Login</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: ''
+      }
+    },
+    methods: {
+      login() {
+        // Make a request to the server to check the provided credentials
+        // and handle the response accordingly
+        // 
+        // Example:
+        // axios.post('/login', {username: this.username, password: this.password})
+        // .then(response => {
+        //  if(response.data.success) {
+        //    // handle successful login
+        //  } else {
+        //    // handle failed login
+        //  }
+        // })
+        // .catch(error => {
+        //  // handle error
+        // });
+        console.log('ok')
+      }
+    }
+  }
+  </script>
+  
